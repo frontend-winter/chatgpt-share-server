a29febf2b5388b06aae9ff7799c8ecefd1ef68dd
winter <huangzhidong@langlang.net.cn>
2024-04-30 17:56:21 +0800

    feat: first commit

--- quick-install.sh
+++ quick-install.sh
@@ -3,7 +3,8 @@ set -e
 
 ## 克隆仓库到本地
 echo "clone repository..."
-git clone -b deploy  --depth=1 https://github.com/xyhelper/chatgpt-share-server.git chatgpt-share
+#git clone -b deploy  --depth=1 https://github.com/frontend-winter/chatgpt-share-server.git chatgpt-share
+git clone -b deploy  --depth=1 https://github.com/frontend-winter/chatgpt-share-server.git chatgpt-share
 
 ## 进入目录
 cd chatgpt-share
